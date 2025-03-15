3ceb462ed715e616293ef1c40ad00735ba6892e9
Saurabh Sharma <sasharma643@gmail.com>
2025-03-16 00:02:55 +0530

    Added user model by extending the base user model for otp & email

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+import random
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
