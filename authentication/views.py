3d9be8d46b9d4a8ed2da55737e043a5aa4463abd
Saurabh Sharma <sasharma643@gmail.com>
2025-03-16 15:09:06 +0530

    Added the LoginView

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,5 @@
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate, login, logout
 import random
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,7 +7,6 @@ from rest_framework.views import APIView
 from .serializers import *
 from .models import User
 from backend import settings
-
 
 
 
@@ -48,3 +48,18 @@ class VerifyOTPView(APIView):
                 return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(email=email, password=password)
+            if user and user.is_verified:
+                login(request, user)
+                response = Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+                response.set_cookie('auth_token', 'sessionid', httponly=True, secure=True)
+                return response
+            return Response({'error': 'Invalid credentials or user not verified'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
