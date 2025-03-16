c8cb9d9a537cf0d4fda95e633d2c84f1bce15949
Saurabh Sharma <sasharma643@gmail.com>
2025-03-16 15:49:03 +0530

    Added session authentication in rest_framework

--- backend/settings.py
+++ backend/settings.py
@@ -93,10 +93,7 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    )
 }
 
 # Password validation
