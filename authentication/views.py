24e1c43e42182f771457d6f2ed880e8d0fb0baa3
Saurabh Sharma <sasharma643@gmail.com>
2025-03-16 15:33:05 +0530

    Added LogoutView and UserDetailsView

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,16 @@
 from django.core.mail import send_mail
 from django.contrib.auth import authenticate, login, logout
-import random
+
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from .serializers import *
+from rest_framework.permissions import IsAuthenticated
+
 from .models import User
+from .serializers import *
 from backend import settings
 
+import random
 
 
 class RegisterView(APIView):
@@ -30,8 +33,7 @@ class RegisterView(APIView):
 
             return Response({'message': 'OTP sent to your email for verification'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    
+      
 class VerifyOTPView(APIView):
     def post(self, request):
         serializer = OTPVerificationSerializer(data=request.data)
@@ -48,7 +50,6 @@ class VerifyOTPView(APIView):
                 return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data)
@@ -63,3 +64,17 @@ class LoginView(APIView):
                 return response
             return Response({'error': 'Invalid credentials or user not verified'}, status=status.HTTP_400_BAD_REQUEST)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        response = Response({'message': 'Logged out'}, status=status.HTTP_200_OK)
+        response.delete_cookie('auth_token')
+        return response
+
+class UserDetailsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
