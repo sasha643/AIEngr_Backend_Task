1b9b5fa7e87b8960b2fe9df2cf0b5352f06ee47a
Saurabh Sharma <sasharma643@gmail.com>
2025-03-16 16:03:04 +0530

    Registered Uer model in admin.py

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'email', 'is_verified', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    list_filter = ('is_verified', 'is_staff', 'is_active')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+        ('Verification', {'fields': ('is_verified', 'otp')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+    filter_horizontal = ()
